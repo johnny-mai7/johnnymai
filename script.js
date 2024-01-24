cf4fa369cd102721555e99adc0953c527d7fe6e2
johnny-mai7 <150837599+johnny-mai7@users.noreply.github.com>
2024-01-23 20:56:12 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function sendMail(params){
         subject_sender: document.getElementById("subjectSender").value,
         message_sender: document.getElementById("message").value,
     };
-    emailjs.send('{{SERVICE ID}}','{{TEMPLATE ID}}',tempParams)
+    emailjs.send('service_gt8aypl','{{TEMPLATE ID}}',tempParams)
      .then(function(res){
         Swal.fire({
             position: 'center',
